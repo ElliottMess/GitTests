d0a189777509bce5de24309b41ce26ae890cd9d3
Elliott Messeiller <elliott.messeiller@gmail.com>
2019-07-12 18:24:39 +0000

    del_dels

--- gitTests.R
+++ gitTests.R
@@ -1,7 +1,8 @@
 initial_commit <- "Initial Commit"
 second_commit <- "This is a second commit"
 
-add_adds <- function(x,y){
-  add_adds <- paste(x,y)
-  return(add_adds)
+del_dels <- function(x,y){
+  add_adds <- paste0(x,y)
+  del_dels <- gsub(x, "", add_adds)
+  return(del_dels)
 }
